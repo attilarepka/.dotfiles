533f07f5cefe916ea6a58d1cf453e7ee6d2ddd38
Attila Repka <repka.attila@yandex.com>
2023-04-10 11:59:15 +0200

    fix: install.sh & refactor: clang

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Define the dotfiles directory
+dotfiles_dir="$HOME/git/.dotfiles"
+
 # Create symbolic links for each dotfile
-ln -sf ./clangd/.clangd-format $HOME/.clangd-format
-ln -sf ./clangd/.clangd-tidy $HOME/.clangd-tidy
-ln -sf ./nvim $HOME/.config/nvim
-ln -sf ./zsh/.zshrc $HOME/.zshrc
-ln -sf ./tmux/.tmux.conf $HOME/.tmux.conf
+ln -sf $dotfiles_dir/clang/.clang-format $HOME/.clang-format
+ln -sf $dotfiles_dir/clang/.clang-tidy $HOME/.clang-tidy
+ln -sf $dotfiles_dir/nvim $HOME/.config/nvim
+ln -sf $dotfiles_dir/zsh/.zshrc $HOME/.zshrc
+ln -sf $dotfiles_dir/tmux/.tmux.conf $HOME/.tmux.conf
 # Add more dotfiles as necessary
 
 echo "Dotfiles installation complete!"
